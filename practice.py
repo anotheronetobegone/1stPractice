c487d61025754b023798d46f1346b0a043c77a8e
anotheronetobegone <anotheronetobegone@gmail.com>
2026-07-08 15:26:50 +0530

    pylint improvement 2 - snake case instead of camel case

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
-"""This script contains functions to check if a number is prime, even, and if a string is a palindrome."""
+"""This script contains functions to check if a number is 
+prime, even, and if a string is a palindrome."""
 
-def isPrime(n):
+def is_prime(n):
     """Checks whether a number is prime or not."""
     if n <= 1:
         return False
@@ -10,12 +11,12 @@ def isPrime(n):
     return True
 
 
-def isEven(n):
+def is_even(n):
     """Checks whether a number is even or not."""
     return n % 2 == 0
 
 
-def isPalindrome(s):
+def is_palindrome(s):
     """Checks whether a string is a palindrome or not."""
     return s == s[::-1]
 
@@ -23,6 +24,6 @@ def isPalindrome(s):
 num = int(input("Enter a number: "))
 text = input("Enter a string/number for palindrome check: ")
 
-print("Prime:", isPrime(num))
-print("Even:", isEven(num))
-print("Palindrome:", isPalindrome(text))
+print("Prime:", is_prime(num))
+print("Even:", is_even(num))
+print("Palindrome:", is_palindrome(text))
